27f90cdfb7cb7c256b72db1740c0df1e648cf78a
Shabbir Akolawala <sakolawala@webuy.com>
2017-04-26 11:04:05 +0530

    Initial commit for test project

---
+++ README.md
@@ -0,0 +1,2 @@
+# testmvc1
+This is just demo repository to test the integration of jenkins with aspnet core project
---
+++ mymvc.test/Controller/TestHomeController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using myMVC.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace mymvc.test.Controller
+{
+    public class TestHomeController
+    {
+        [Fact]
+        public void TestIndexMethod()
+        {
+            var controller = new HomeController();
+            var result = controller.Index() as ViewResult;
+            Assert.Equal("Index", result.ViewName);
+        }
+
+        [Fact]
+        public void TestAboutMethod()
+        {
+            var controller = new HomeController();
+            var result = controller.About() as ViewResult;
+            Assert.Equal("About", result.ViewName);
+        }
+    }
+}
