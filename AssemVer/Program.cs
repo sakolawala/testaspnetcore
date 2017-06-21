463251bfc31aaeb6f8ca5676602fd087290591f8
Shabbir Akolawala <sakolawala@webuy.com>
2017-06-21 10:52:46 +0530

    Added new project to read assembly version

---
+++ AssemVer/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace AssemVer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {                      
+            var myAssembly = System.Runtime.Loader.AssemblyLoadContext.Default.LoadFromAssemblyPath(@"F:\Shabs\prj\rnd\dc\testaspnetcore\myMVC\bin\Debug\netcoreapp1.1.1\myMVC.dll");
+            var versionstr = myAssembly.GetName().Version.ToString();
+            string fileVersion = FileVersionInfo.GetVersionInfo(myAssembly.Location).FileVersion;
+            string productVersion = FileVersionInfo.GetVersionInfo(myAssembly.Location).ProductVersion;
+            Console.WriteLine($"Simple Version: {versionstr}");
+            Console.WriteLine($"File Version: {fileVersion}");
+            Console.WriteLine($"Product Version: {productVersion}");
+            Console.ReadLine();
+        }
+    }
+}
