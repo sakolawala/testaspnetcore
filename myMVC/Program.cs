3c4e20fd8cfb6ed85f89e60095814a6195870043
Shabbir Akolawala <sakolawala@webuy.com>
2017-06-26 12:56:03 +0530

    Remove application insight which has incompatible package

--- myMVC/Program.cs
+++ myMVC/Program.cs
@@ -16,7 +16,6 @@ namespace myMVC
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
                 .UseStartup<Startup>()
-                .UseApplicationInsights()
                 .Build();
 
             host.Run();
